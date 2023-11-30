928cbc72a701b1d94a31d78d6ab306200aed15ca
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-11-29 17:13:07 -0800

    added render function, renamed hamilton product function, and now uses unsigned ints

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ float axial_cos(const quaternion q1, const quaternion q2) {
     return dot_product(q1, q2) / (magnitude(q1) * magnitude(q2));
 }
 
-quaternion multiply(const quaternion q1, const quaternion q2) {
+quaternion hamilton_product(const quaternion q1, const quaternion q2) {
     return (quaternion) {q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z,
                          q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y,
                          q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x,
@@ -41,20 +41,25 @@ quaternion multiply(const quaternion q1, const quaternion q2) {
 
 const quaternion from_euler(const quaternion axis, const float angle) {
     float half_angle = angle / 2;
+	float sin_half_angle = sin(half_angle);
     return (quaternion) {cos(half_angle),
-                         sin(half_angle) * axial_cos(axis, x_axis),
-                         sin(half_angle) * axial_cos(axis, y_axis),
-                         sin(half_angle) * axial_cos(axis, z_axis)};
+                         sin_half_angle * axial_cos(axis, x_axis),
+                         sin_half_angle * axial_cos(axis, y_axis),
+                         sin_half_angle * axial_cos(axis, z_axis)};
 }
 
 quaternion rotate(const quaternion vector, const quaternion rotation) {
-    return multiply(multiply(rotation, vector), conjugate(rotation));
+    return hamilton_product(hamilton_product(rotation, vector), conjugate(rotation));
+}
+
+char render(const int brightness) {
+	return ".,-~:;!=*#$@"[brightness];
 }
 
 int main(void) {
     const float two_pi = 2 * M_PI, max_shift = 0.005;
-    const int radius = 8;
-    const int display_size = 5 * radius, radius_squared = radius * radius;
+    const unsigned int radius = 8;
+    const unsigned int display_size = 5 * radius, radius_squared = radius * radius;
     const float delta_theta = two_pi / 100, delta_phi = two_pi / 100;
     const quaternion light_source = {0, 0, 0, -1};
     float x_angle = 0, y_angle = 0, z_angle = 0;
@@ -63,7 +68,7 @@ int main(void) {
         memset(display_buffer, (float) display_size, sizeof display_buffer);
         char display[display_size][display_size];
         memset(display, ' ', sizeof display);
-        const quaternion rotation = multiply(multiply(from_euler(x_axis, x_angle),
+        const quaternion rotation = hamilton_product(hamilton_product(from_euler(x_axis, x_angle),
                                                   from_euler(y_axis, y_angle)),
                                          from_euler(z_axis, z_angle));
         for (float theta = 0; theta < two_pi; theta += delta_theta) {
@@ -88,19 +93,19 @@ int main(void) {
                     const float z_buffer = position.z + display_size;
                     const int half_display = display_size / 2;
                     const float z_inverse = 1 / (z_buffer);
-                    const int x = (int) half_display *
+                    const unsigned int x = (unsigned int) half_display *
                                                    (1 + position.x * z_inverse),
-                              y = (int) half_display *
+                              		   y = (unsigned int) half_display *
                                                    (1 + position.y * z_inverse);
                     if (position.z <= display_buffer[y][x]) {
                         display_buffer[y][x] = position.z;
-                        display[y][x] = ".,-~:;!=*#$@"[brightness];
+                        display[y][x] = render(brightness);
                     }
                 }
             }
         }
-        for (int i = 0; i < display_size; ++i) {
-            for (int j = 0; j < display_size; ++j) {
+        for (unsigned int i = 0; i < display_size; ++i) {
+            for (unsigned int j = 0; j < display_size; ++j) {
                 printf("%c ", display[i][j]);
             }
             printf("\n");
